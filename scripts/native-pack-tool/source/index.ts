7611668235ba71d0d577490e22c75e6613b96865
qiuguohua <qiuguohua111@126.com>
2024-11-29 09:15:33 +0800

    Change openharmony to harmonyos-next (#17988)
    Co-authored-by: qiuguohua <guohua.qiu@cocos.com>

--- scripts/native-pack-tool/source/index.ts
+++ scripts/native-pack-tool/source/index.ts
@@ -1,9 +1,8 @@
-
 import { nativePackToolMg } from './base/manager';
 import { MacPackTool } from './platforms/mac';
 import { WindowsPackTool } from './platforms/windows';
 import { AndroidPackTool } from './platforms/android';
-import { OpenHarmonyPackTool } from './platforms/openharmony';
+import { HarmonyOSNextPackTool } from './platforms/harmonyos-next';
 import { OHOSPackTool } from './platforms/ohos';
 import { IOSPackTool } from './platforms/ios';
 import { HuaweiAGCPackTool } from './platforms/huawei-agc';
@@ -12,7 +11,7 @@ nativePackToolMg.register('ios', new IOSPackTool());
 nativePackToolMg.register('mac', new MacPackTool());
 nativePackToolMg.register('windows', new WindowsPackTool());
 nativePackToolMg.register('android', new AndroidPackTool());
-nativePackToolMg.register('openharmony', new OpenHarmonyPackTool());
+nativePackToolMg.register('harmonyos-next', new HarmonyOSNextPackTool());
 nativePackToolMg.register('ohos', new OHOSPackTool());
 nativePackToolMg.register('huawei-agc', new HuaweiAGCPackTool());
 
